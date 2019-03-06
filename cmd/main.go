853df5b092136da9c6998a0018093243f6a8e712
Hafiz Joundy Syafie <hafizjoundys@gmail.com>
2019-03-07 01:36:38 +0700

    add viper for cofiguration file

--- cmd/main.go
+++ cmd/main.go
@@ -2,19 +2,38 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	_postRepo "github.com/haffjjj/myblog-backend/repository/post"
 	_postUsecase "github.com/haffjjj/myblog-backend/usecase/post"
 	"github.com/labstack/echo"
+	"github.com/spf13/viper"
 
 	_httpDelivery "github.com/haffjjj/myblog-backend/delivery/http"
 	"github.com/mongodb/mongo-go-driver/mongo"
 )
 
+func init() {
+	viper.SetConfigFile("config")
+	viper.SetConfigType("json")
+	viper.SetConfigFile("../config/config.json")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
 
-	mgoClient, err := mongo.Connect(context.TODO(), "mongodb://localhost:27017")
+	var (
+		dbHost = viper.GetString("database.mongodb.host")
+		dbPort = viper.GetString("database.mongodb.port")
+		port   = viper.GetString("port")
+	)
+
+	mgoClient, err := mongo.Connect(context.TODO(), fmt.Sprint("mongodb://", dbHost, dbPort))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -30,5 +49,5 @@ func main() {
 	e := echo.New()
 	_httpDelivery.NewPostHandler(e, postUsecase)
 
-	e.Logger.Fatal(e.Start(":8081"))
+	e.Logger.Fatal(e.Start(port))
 }
