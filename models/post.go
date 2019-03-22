6288545bde5dcece7cb0e5c66b4dbd50a3fc5293
Hafiz Joundy Syafie <hafizjoundys@gmail.com>
2019-03-23 01:24:48 +0700

    update models post

--- models/post.go
+++ models/post.go
@@ -1,13 +1,18 @@
 package models
 
+import (
+	"github.com/mongodb/mongo-go-driver/bson/primitive"
+)
+
 // Post represent model for post data
 type Post struct {
-	Title       string   `json:"title" bson:"title" validate:"required"`
-	Thumbnail   string   `json:"thumbnail" bson:"thumbnail" validate:"required"`
-	CreatedAt   string   `json:"createdAt" bson:"createdAt" validate:"required"`
-	ReadingTime string   `json:"readingTime" bson:"readingTime" validate:"required"`
-	Tag         []string `json:"tag" bson:"tag" validate:"required"`
-	Content     string   `json:"content" bson:"content" validate:"required"`
+	ID          primitive.ObjectID `json:"id" bson:"_id"`
+	Title       string             `json:"title" bson:"title" validate:"required"`
+	Thumbnail   string             `json:"thumbnail" bson:"thumbnail" validate:"required"`
+	CreatedAt   string             `json:"createdAt" bson:"createdAt" validate:"required"`
+	ReadingTime string             `json:"readingTime" bson:"readingTime" validate:"required"`
+	Tag         []string           `json:"tag" bson:"tag" validate:"required"`
+	Content     string             `json:"content" bson:"content" validate:"required"`
 }
 
 //PostsGroup represent for postGroup data
